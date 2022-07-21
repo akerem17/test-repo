3110593bd2383e853e0f4bf18dd78f130772fd1a
Ahmet Kerem <ahmetkerem.dev@gmail.com>
2022-07-21 17:44:36 +0300

    added button & scripts

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("sa")
+
+function press(number) {
+  if (!number) 
+    console.log("Number verilmedi.");
+  else if (number === 1) 
+    console.log("Emin misin? 1.");
+  if (number === 2) 
+    console.log("İkide iki!");
+}
